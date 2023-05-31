10c8cd5928b40da1fbf0e6b025dc5b9305b554b0
Demonstr66 <Demonstr66@yandex.ru>
2023-06-01 00:06:07 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = new express()
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+  console.log('123')
   res.json({message: "HELLO TEAMWATCHER FROM SERVER!"})
+})
+
+
+app.get('/api/*', (req, res) => {
+  console.log('fff')
+  res.json({message: "HELLO TEAMWATCHER FROM SERVER API!"})
 })
 
 app.listen(PORT, () => {
