d77e6f6d096a71e14fd633dd277794285089ae01
Demonstr66 <Demonstr66@yandex.ru>
2023-06-04 00:26:39 +0300

    try add sql

--- src/index.js
+++ src/index.js
@@ -2,14 +2,39 @@ const express = require('express');
 const app = new express()
 const PORT = process.env.PORT || 3000;
 
+const {Client} = require('pg')
+console.log(Client)
+const client = new Client({
+  host: 'localhost',
+  port: 35432,
+  database: 'postgres',
+  user: 'postgres',
+  password: 'example',
+})
+client.connect()
+
+
+
 app.get('/', (req, res) => {
   console.log('123')
   res.json({message: "HELLO TEAMWATCHER FROM SERVER!"})
 })
 
 
+
+app.get('/api/sql', async (req, res) => {
+  try {
+    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message) // Hello world!
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end()
+  }
+})
+
+
 app.get('/api/*', (req, res) => {
-  console.log('fff')
   res.json({message: "HELLO TEAMWATCHER FROM SERVER API!"})
 })
 
